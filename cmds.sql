d48cd50c62349a654be14680ec3dd7820fd0cb61
Melwin wilson <102460667+Quaker69@users.noreply.github.com>
2024-11-27 15:58:27 +0530

    jhhfasdjads
    new

---
+++ cmds.sql
@@ -0,0 +1,4 @@
+create database control_de_stock;
+use control_de_stock;
+CREATE TABLE PRODUCTO (id INT AUTO_INCREMENT PRIMARY KEY,nombre VARCHAR(255) NOT NULL,descripcion VARCHAR(500),cantidad
+INT NOT NULL );
