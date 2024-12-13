ad6f0e5de12d3f2c9409b033bb02c40e28d35242
Melwin wilson <102460667+Quaker69@users.noreply.github.com>
2024-12-14 01:57:13 +0530

    FINAL PUSH
    Co-Authored-By: M9902595915m <170664469+M9902595915m@users.noreply.github.com>

--- src/main/java/com/inventory/jdbc/databases_stuff_do_here/ConnectionFactory.java
+++ src/main/java/com/inventory/jdbc/databases_stuff_do_here/ConnectionFactory.java
@@ -2,41 +2,41 @@ package com.inventory.jdbc.databases_stuff_do_here;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-
 import javax.sql.DataSource;
-
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConnectionFactory {
 
-	private String user = "root";
+    private String user = "root";
+    private String password = "harsha";
+    private DataSource dataSource;
 
-	private String password = "harsha";
-	
-	private DataSource dataSource;
-	
+    // Static block to disable all logging
+    static {
+        Logger mlogLogger = Logger.getLogger("com.mchange.v2.log.MLog");
+        mlogLogger.setLevel(Level.OFF); // Disable logging for MLog
 
-	public ConnectionFactory(){
-		var pooledDatraSource = new ComboPooledDataSource();
-		pooledDatraSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock");
-		pooledDatraSource.setUser(user);
-		pooledDatraSource.setPassword(password);
-		pooledDatraSource.setMaxPoolSize(10);
-		this.dataSource = pooledDatraSource;
-	}
-	
-	public Connection recoverConnection() {
+        Logger c3p0Logger = Logger.getLogger("com.mchange.v2.c3p0");
+        c3p0Logger.setLevel(Level.OFF); // Disable logging for c3p0
+    }
 
-		
-		try {
+    public ConnectionFactory(){
+        var pooledDatraSource = new ComboPooledDataSource();
+        pooledDatraSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock");
+        pooledDatraSource.setUser(user);
+        pooledDatraSource.setPassword(password);
+        pooledDatraSource.setMaxPoolSize(10);
+        this.dataSource = pooledDatraSource;
+    }
 
-			return this.dataSource.getConnection();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-			
-		}
-	}
-
+    public Connection recoverConnection() {
+        try {
+            return this.dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
